63771b781a9827cf6172c819d2f490d5c3e04e1a
unknown <diego.resendez@yahoo.com>
2020-07-30 18:02:39 -0700

    Added Server Folder to hold back end programming

---
+++ Server/Models/Workout.js
@@ -0,0 +1,26 @@
+// import mongoose from 'mongoose';
+
+const mongoose = require("mongoose");
+const 
+ 
+const workout = new mongoose.Schema(
+    {
+        exercises: [
+          {
+            type: String,
+            name: String,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            unique: true,
+            required: true,
+          }
+        ]
+      },
+  { timestamps: true },
+);
+ 
+const Workout = mongoose.model('User', workout);
+ 
+module.exports = Workout;
